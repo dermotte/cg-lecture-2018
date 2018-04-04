a0c9d9c0b276388974bd2f626c1b522f96af0d80
Mathias Lux <mathias.lux@aau.at>
2018-04-04 15:19:19 +0200

    shaders: phong

--- 05/sketch_01_shaders/data/PhongFrag.glsl
+++ 05/sketch_01_shaders/data/PhongFrag.glsl
@@ -6,8 +6,9 @@ varying vec3 vertLightDir;
 void main() {
   float amb = 0.1;
 
-  vec3 reflectionDir = normalize(-reflect(-vertLightDir,vertNormal));
-  vec4 spec = vec4(1.0, 1.0, 1.0, 1.0) * pow(max(dot(reflectionDir,-vertLightDir),0.0), 20) ;
+  // vec3 reflectionDir = normalize(-reflect(-vertLightDir,vertNormal));
+  vec3 reflectionDir = 2 * dot(-vertLightDir, vertNormal) * vertNormal + vertLightDir; // that's actually a reflection!
+  vec4 spec = 0.2 * vec4(1.0, 1.0, 1.0, 1.0) * pow(max(dot(reflectionDir,-vertLightDir),0.0), 5) ;
   float diff = max(0.0, dot(vertLightDir, vertNormal)) * (1.0-amb);
   gl_FragColor = vec4(diff, diff, diff, 1.0-amb) * vertColor
     + spec
